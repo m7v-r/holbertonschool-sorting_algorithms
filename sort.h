d6ea6d2d5ebdf92ee44b1f45245fc4da9dedad11
m7v-r <ms4571690@icloud.com>
2026-05-09 16:25:11 +0000

    Fix Betty documentation in sort.h

--- sort.h
+++ sort.h
@@ -6,6 +6,10 @@
 
 /**
  * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
@@ -14,12 +18,12 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Helper functions */
+/* Helper functions provided */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* Prototypes */
+/* Prototypes for tasks */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 
-#endif
+#endif /* SORT_H */
